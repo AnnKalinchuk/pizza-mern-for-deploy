cb46c11367584f463750b0396175d5e2aaa70971
Anna Kalynchuk <anya.kalinchuk.94@gmail.com>
2023-12-22 19:11:01 +0100

    added cors rule

--- server/middleware/corsMiddleware.js
+++ server/middleware/corsMiddleware.js
@@ -1,5 +1,6 @@
 function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', 'https://pizza-mern-for-deploy-client.vercel.app');
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
    /*  res.header("Access-Control-Allow-Headers", "Content-Type"); */
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
