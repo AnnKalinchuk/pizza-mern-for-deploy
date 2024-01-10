790aae91bfa65f4622f4da72fb7181277f5e22e8
Anna Kalynchuk <anya.kalinchuk.94@gmail.com>
2024-01-10 18:31:39 +0100

    2024

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className={classes.footer}>
             <div className={classes.footer__content}>
-                <div>© 2022 PIZZAnna Ukraine</div>
+                <div>© 2024 PIZZAnna Ukraine</div>
                 <div>Contacts example: +380(99)-000-00-00</div>
             </div>
         </div>
